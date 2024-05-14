48b758c39391ddd00a91b80a23ea7458220555a8
KumJungMin <rmawjdals@naver.com>
2024-05-14 20:58:26 +0900

    2. [최적화] Automatic batching로 상태 업데이트를 한번으로 결합

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import App from "./App";
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 
+// React 18에서는 createRoot를 사용해 렌더링하면, Automatic batching이 활성화됩니다. 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 

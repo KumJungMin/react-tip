de8992c48626a0ee36b3b6e144ebe63558c139e0
KumJungMin <rmawjdals@naver.com>
2024-05-16 20:03:17 +0900

    5. [최적화] 여러 리스트는 화면에 보이는 부분만 렌더링하기(대체: react-window)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import {useEffect, useState} from 'react'
 import './App.css'
+import RecyclerView from "./RecyclerView.tsx";
 
 function App() {
     const [items, setItems] = useState<string[]>([]);
 
     useEffect(() => {
-      const newItems = [];
-      for (let i = 0; i < 100000; i++) {
-          newItems.push(`Item ${i}`);
-      }
-      setItems(newItems);
+        const newItems = [];
+        for (let i = 0; i < 100000; i++) {
+            newItems.push(`Item ${i}`);
+        }
+        setItems(newItems);
     }, []);
 
     return (
-      <div className="App">
-        <ul>
-          {items.map((item, index) => (
-              <li key={index}>{item}</li>
-          ))}
-        </ul>
-      </div>
+        <div className="App">
+            <RecyclerView
+                itemCount={items.length}
+                items={items}
+                renderItem={(item) => (<div>{item}</div>)}
+                itemSize={100}
+            />
+        </div>
     );
 }
 

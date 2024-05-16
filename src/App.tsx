94e23df37779a8f141fa8ea53c3a425d0466822b
KumJungMin <rmawjdals@naver.com>
2024-05-16 19:26:28 +0900

    feat: 예시 코드 추가하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,61 @@
-import "./App.css";
-import React, { useEffect } from "react";
+import { memo, useEffect, useState } from "react";
 
-const getUsers = () =>
-  new Promise((resolve) => setTimeout(() => resolve(), 1000));
+const targetURL =
+    "https://raw.githubusercontent.com/jason-chao/wordle-solver/main/english_words_original_wordle.txt";
 
-export default function App() {
-  const [count1, setCount1] = React.useState(0);
-  const [count2, setCount2] = React.useState(0);
-
-  /** 
-   * React 18에서 도입된 Automatic Batching은 여러 상태 업데이트를 자동으로 하나의 렌더링 작업으로 결합하여 효율성을 높이는 기능
-   * Automatic batching이 활성화되면, useEffect에서 count1과 count2가 동시에 업데이트
-   **/
-  useEffect(() => {
-    console.log("count1", count1);
-    console.log("count2", count2);
-  }, [count1, count2]);
-
-  /**
-   * React 18 이전 버전에서는 이벤트 핸들러 내부의 상태 업데이트만 배치 처리가 됨
-   * React 18부터는 비동기 작업 내의 상태 업데이트도 자동으로 배치
-   * -> 불필요한 재렌더링을 줄이고 성능을 향상시킬 수 있음
-   */ 
-  const onClick = async () => {
-    await getUsers();
-    setCount1(count1 + 1);
-    setCount2(count2 + 1);
-  };
-
+const Word = memo(({ name, highlight = "" }) => {
+  const letters = name.split("");
   return (
-    <div className="App">
-      <h1>Counter</h1>
-      <h2>Count1: {count1}</h2>
-      <h2>Count2: {count2}</h2>
-      <button onClick={onClick}>Click</button>
-    </div>
+      <li>
+          {letters.map((l, i) => (
+              <span className={highlight.includes(l) ? "h" : ""} key={i}>
+        {l}
+      </span>
+          ))}
+      </li>
+  );
+}, (prevProps, nextProps) => {
+  const letters = prevProps.name.split("");
+  return letters.every(l => {
+      return nextProps.highlight.includes(l) === prevProps.highlight.includes(l);
+  })
+})
+
+const Words = ({list, filter}) => {
+  return (
+    <ul>
+      {list.map((name) => (
+        <Word key={name} name={name} highlight={filter}/>
+      ))}
+    </ul>
+  );
+};
+
+function FilterWords() {
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    fetch(targetURL)
+      .then((r) => r.text())
+      .then((r) => setList(r.split("\n").sort()));
+  }, []);
+  const [filter, setFilter] = useState("");
+
+  const handleChange = ({target: {value}}) => {
+    setFilter(value);
+  };
+  return (
+    <main>
+      <label>
+        Filter:
+        <input type="search" value={filter} onChange={handleChange}/>
+      </label>
+      <Words list={list} filter={filter}/>
+    </main>
   );
 }
+
+function App() {
+  return <FilterWords/>;
+}
+
+export default App;

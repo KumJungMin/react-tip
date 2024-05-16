665434bb25eb12ae074475915b3bde634ad52f25
KumJungMin <rmawjdals@naver.com>
2024-05-16 19:53:47 +0900

    4. [최적화] browser-image-compression로 이미지 압축하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,32 @@
-import { memo, useEffect, useState } from "react";
+import "./styles.css";
+import React from "react";
+import imageCompression from "browser-image-compression";
 
-const targetURL =
-    "https://raw.githubusercontent.com/jason-chao/wordle-solver/main/english_words_original_wordle.txt";
+export default function App() {
+  const [url, setUrl] = React.useState();
 
-const Word = ({ name, highlight = "" }) => {
-  const letters = name.split("");
-  return (
-    <li>
-      {letters.map((l, i) => (
-        <span className={highlight.includes(l) ? "h" : ""} key={i}>
-          {l}
-        </span>
-      ))}
-    </li>
-  );
-}
+  const onChange = async (e) => {
+    if (e.target.files) {
+      const reader = new FileReader();
 
-// 모든 글자가 highlight에 포함되어 있는지 확인
-// 모두 포함되어 있다면, 이전 props와 다음 props는 같은 것으로 간주
-// 만약 이전 props와 다음 props가 같다면, 렌더링을 하지 않음
-const wordMemoFunc = (prev, next) => {
-  return prev.name.split("").every(l => {
-    return next.highlight.includes(l) === prev.highlight.includes(l);
-  });
-}
+      reader.onload = function (event) {
+        setUrl(event.target.result);
+      };
 
-/** 
- * memo?
- * 용법: memo(Component, areEqual)
- * 설명: React.memo는 컴포넌트를 렌더링하는 동안 props가 변경되지 않으면 이전 결과를 재사용합니다.
- * 
- * areEqual: (prevProps, nextProps) => boolean
- * areEqual이 true이면 이전 결과를 재사용합니다.(렌더링을 하지 않음)
- * 
- * 장점: 렌더링 성능을 최적화할 수 있습니다.
- * 유용한 경우: 렌더링이 자주 발생하거나, 렌더링 비용이 큰 컴포넌트
-*/
-const MemorizedWord = memo(Word, wordMemoFunc);
+      const compress = await imageCompression(e.target.files[0], {
+        initialQuality: 0.1
+      });
 
-
-const Words = ({list, filter}) => {
-  return (
-    <ul>
-      {list.map((name) => (
-        <MemorizedWord key={name} name={name} highlight={filter}/>
-      ))}
-    </ul>
-  );
-};
-
-function FilterWords() {
-  const [list, setList] = useState([]);
-  useEffect(() => {
-    fetch(targetURL)
-      .then((r) => r.text())
-      .then((r) => setList(r.split("\n").sort()));
-  }, []);
-  const [filter, setFilter] = useState("");
-
-  const handleChange = ({target: {value}}) => {
-    setFilter(value);
+      reader.readAsDataURL(compress);
+      console.log("original", Math.ceil(e.target.files[0].size / 1000), "KB");
+      console.log(Math.ceil(compress.size / 1000), "KB");
+    }
   };
+
   return (
-    <main>
-      <label>
-        Filter:
-        <input type="search" value={filter} onChange={handleChange}/>
-      </label>
-      <Words list={list} filter={filter}/>
-    </main>
+    <div className="App">
+      <input onChange={onChange} type="file"></input>
+      <img src={url} width={500} height={500} />
+    </div>
   );
 }
-
-function App() {
-  return <FilterWords/>;
-}
-
-export default App;
